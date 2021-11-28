ad61b81dcafd852d15bb536c5381b86641112001
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-11-27 23:09:04 -0500

    cleaned up examples

--- examples/kick_ball_sim.jl
+++ examples/kick_ball_sim.jl
@@ -1,43 +1,47 @@
 using contact_sim  
 
-#walker inits 
-q1 = 0.2; u1 = -0.25;
-q2 = -0.4; u2 = 0.2;
-z0 = [q1 u1 q2 u2]
-steps = 4
-fps = 20
-walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, true, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
-z_init = [ 0.142819999999995  -0.326813112785275  -0.285640000000000   0.068243824367047]
-zpert = z_init + [0. 0.05 -0.1 0.2]
+function main()
+    #walker inits 
+    q1 = 0.2; u1 = -0.25;
+    q2 = -0.4; u2 = 0.2;
+    z0 = [q1 u1 q2 u2]
+    steps = 4
+    fps = 20
+    walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, true, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
+    z_init = [ 0.142819999999995  -0.326813112785275  -0.285640000000000   0.068243824367047]
+    zpert = z_init + [0. 0.05 -0.1 0.2]
 
-#ball inits 
-l = 0.4 
-μ = 0.3
-ϵ = 0.5
-dt = 0.01
-m = 0.04
-g = [0., -9.81, 0.]
-R = (1.0/12.) * (2 * l * l) 
-M = [[m 0. 0.]; 
-    [0. m 0.];
-    [0. 0. m * R]] 
-Mi = [[1.0/m  0.  0.];
-    [0. 1.0/m 0.];
-    [0. 0. 1.0/(m*R)]]
-Δ = 0.001
-T = 150 
-q₀ = [0.0, 0.2, π/180.0*30.] 
+    #ball inits 
+    l = 0.4 
+    μ = 0.3
+    ϵ = 0.5
+    dt = 0.01
+    m = 0.04
+    g = [0., -9.81, 0.]
+    R = (1.0/12.) * (2 * l * l) 
+    M = [[m 0. 0.]; 
+        [0. m 0.];
+        [0. 0. m * R]] 
+    Mi = [[1.0/m  0.  0.];
+        [0. 1.0/m 0.];
+        [0. 0. 1.0/(m*R)]]
+    Δ = 0.001
+    T = 150 
+    q₀ = [0.0, 0.2, π/180.0*30.] 
 
-Torque = 27.9 #hinge motor torque (Nm)
-Fe = Torque*walker.l #kick force (N)
-th = 0.24 #kick angle (rad)
-v₀ = dt*Mi*[Fe*cos(th); Fe*sin(th); 0.0] 
+    Torque = 27.9 #hinge motor torque (Nm)
+    Fe = Torque*walker.l #kick force (N)
+    th = 0.24 #kick angle (rad)
+    v₀ = dt*Mi*[Fe*cos(th); Fe*sin(th); 0.0] 
 
 
-body = Object(l, μ, ϵ, m, R, M)
-physics = Physics(dt, g, Δ, T)
+    body = Object(l, μ, ϵ, m, R, M)
+    physics = Physics(dt, g, Δ, T)
 
-zs, ts = simulate_walking(zpert,walker,steps)
-qs, v = simulate(q₀, v₀, body, physics, get_ball_contact_jacobian) 
-render_kick_trajectory(ts, zs, qs, walker, steps, fps, body, physics, true)
+    zs, ts = simulate_walking(zpert,walker,steps)
+    qs, v = simulate(q₀, v₀, body, physics, get_ball_contact_jacobian) 
+    render_kick_trajectory(ts, zs, qs, walker, steps, fps, body, physics, true)
+end
+
+main()
  
--- examples/walker_sim.jl
+++ examples/walker_sim.jl
@@ -1,27 +1,31 @@
 using contact_sim
 
-q1 = 0.2; u1 = -0.25;
-q2 = -0.4; u2 = 0.2;
+function main()
+    q1 = 0.2; u1 = -0.25;
+    q2 = -0.4; u2 = 0.2;
 
-z0 = [q1 u1 q2 u2]
-steps = 4
-fps = 20
+    z0 = [q1 u1 q2 u2]
+    steps = 4
+    fps = 20
 
-#passive: no controller
-#=
-walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, false, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
-z_init = [0.162597833780035  -0.231869638058927  -0.325195667560070   0.03797846807373]
-zpert = z_init
-=#
+    #passive: no controller
+    #=
+    walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, false, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
+    z_init = [0.162597833780035  -0.231869638058927  -0.325195667560070   0.03797846807373]
+    zpert = z_init
+    =#
 
-#active: use controller
-walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, true, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
+    #active: use controller
+    walker = Walker(1.0, 0.5, 0.02, 1.0, 0.5, 1.0, 0.01, true, 1.0, 100.0, 0.28564, z0[3], z0[4], 0.0)
 
-#fixed point initial value. 
-z_init = [ 0.142819999999995  -0.326813112785275  -0.285640000000000   0.068243824367047]
+    #fixed point initial value. 
+    z_init = [ 0.142819999999995  -0.326813112785275  -0.285640000000000   0.068243824367047]
 
-#adding slight perturbation
-zpert = z_init + [0. 0.05 -0.1 0.2]
+    #adding slight perturbation
+    zpert = z_init + [0. 0.05 -0.1 0.2]
 
-z, t = simulate_walking(zpert,walker,steps)
-render_walking_trajectory(t,z,walker, steps, fps,true)
+    z, t = simulate_walking(zpert,walker,steps)
+    render_walking_trajectory(t,z,walker, steps, fps,true)
+end
+
+main()
